408b0533c7c6b10d535bcfa0fc223a506e6031d3
ismeniabrandao <brandaoismenia@gmail.com>
2023-01-02 13:01:32 +0100

    Initial Commit

---
+++ Tre-i-rad iosTests/Tre_i_rad_iosTests.swift
@@ -0,0 +1,33 @@
+//
+//  Tre_i_rad_iosTests.swift
+//  Tre-i-rad iosTests
+//
+//  Created by Ismenia Brandao on 2023-01-02.
+//
+
+import XCTest
+@testable import Tre_i_rad_ios
+
+class Tre_i_rad_iosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
